d19915326f6b58643d94ad11a45cd6befe68516e
Sizhong Du <sidu@microsoft.com>
2022-09-01 18:51:15 +0800

    Update.

--- src/main/java/sidu/poc/App.java
+++ src/main/java/sidu/poc/App.java
@@ -1,17 +1,21 @@
 package sidu.poc;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.*;
 import org.apache.iceberg.catalog.Namespace;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.data.GenericAppenderFactory;
+import org.apache.iceberg.data.GenericRecord;
 import org.apache.iceberg.data.IcebergGenerics;
 import org.apache.iceberg.data.Record;
+import org.apache.iceberg.deletes.EqualityDeleteWriter;
 import org.apache.iceberg.deletes.PositionDelete;
 import org.apache.iceberg.deletes.PositionDeleteWriter;
 import org.apache.iceberg.encryption.EncryptedOutputFile;
 import org.apache.iceberg.expressions.Expressions;
 import org.apache.iceberg.io.CloseableIterable;
+import org.apache.iceberg.io.DataWriter;
 import org.apache.iceberg.io.DeleteSchemaUtil;
 import org.apache.iceberg.io.OutputFileFactory;
 import org.apache.iceberg.jdbc.JdbcCatalog;
@@ -46,7 +50,6 @@ public class App
         LOG.info( "--- END ---" );
     }
 
-    private final String tableName = "contacts5";
     private final JdbcCatalog catalog;
     private final TableIdentifier tableIdentifier;
 
@@ -68,7 +71,7 @@ public class App
         LOG.info("Catalog name: {}", catalog.name());
 
         Namespace nyc = Namespace.of("main");
-        tableIdentifier = TableIdentifier.of(nyc, tableName);
+        tableIdentifier = TableIdentifier.of(nyc, CONFIG.getProperty("tableName"));
     }
 
     public void create() {
@@ -99,13 +102,13 @@ public class App
         LOG.info("Scan all records:");
         CloseableIterable<Record> result = IcebergGenerics.read(table).build();
         for (Record r: result) {
-            LOG.info(r.toString());
+            LOG.info("Path: {}; Position: {}; Content: {}", r.getField(MetadataColumns.FILE_PATH.name()), r.getField(MetadataColumns.ROW_POSITION.name()), r);
         }
 
-        LOG.info("Scan all records where contact_class == A:");
-        result = IcebergGenerics.read(table).where(Expressions.equal("contact_class", "A")).build();
+        LOG.info("Scan all records where contact_id == 9:");
+        result = IcebergGenerics.read(table).where(Expressions.equal("contact_id", 9)).build();
         for (Record r: result) {
-            LOG.info(r.toString());
+            LOG.info("Path: {}; Position: {}; Content: {}", r.getField(MetadataColumns.FILE_PATH.name()), r.getField(MetadataColumns.ROW_POSITION.name()), r);
         }
 
         LOG.info("TableScan with contact_class == A:");
@@ -140,6 +143,33 @@ public class App
                 .withRecordCount(1)
                 .build();
 
+        LOG.info("Append data file");
+        table.newAppend().appendFile(dataFile).commit();
+    }
+
+    public void append2() throws IOException {
+        Table table = catalog.loadTable(tableIdentifier);
+        Schema schema = table.schema();
+        PartitionSpec partitionSpec = table.spec();
+
+        GenericAppenderFactory genericAppenderFactory = new GenericAppenderFactory(schema, partitionSpec);
+
+        OutputFileFactory outputFileFactory =
+                OutputFileFactory.builderFor(table, 998, 999).format(FileFormat.AVRO).build();
+
+        PartitionKey partitionKey = new PartitionKey(partitionSpec, schema);
+        partitionKey.set(0, "A");
+        EncryptedOutputFile outputFile = outputFileFactory.newOutputFile();
+        DataWriter<Record> dataWriter = genericAppenderFactory.newDataWriter(outputFile, FileFormat.AVRO, partitionKey);
+
+        try (DataWriter<Record> closeableWriter = dataWriter) {
+            GenericRecord record = GenericRecord.create(schema);
+            for (int i = 0; i < 10; ++i) {
+                closeableWriter.write(record.copy(ImmutableMap.of("contact_class", "A", "contact_id", i, "contact_name", "Bar" + i)));
+            }
+        }
+
+        DataFile dataFile = dataWriter.toDataFile();
         LOG.info("Append data file");
         table.newAppend().appendFile(dataFile).commit();
     }
@@ -191,6 +221,7 @@ public class App
 
         OutputFileFactory outputFileFactory =
                 OutputFileFactory.builderFor(table, 1, 1).format(FileFormat.PARQUET).build();
+
         PartitionKey partitionKey = new PartitionKey(partitionSpec, schema);
         partitionKey.set(0, "A");
         EncryptedOutputFile outputFile = outputFileFactory.newOutputFile();
@@ -221,6 +252,36 @@ public class App
                 .build();
 
         LOG.info("Row delta - deleteByEquality");
+        table.newRowDelta().addDeletes(deleteFile).commit();
+    }
+
+    public void deleteByEquality2() throws IOException {
+        Table table = catalog.loadTable(tableIdentifier);
+        Schema schema = table.schema();
+        PartitionSpec partitionSpec = table.spec();
+
+        Schema eqDeleteRowSchema = schema.select("contact_class", "contact_id");
+        GenericAppenderFactory genericAppenderFactory = new GenericAppenderFactory(
+                schema, partitionSpec, new int[] { 1, 2 }, eqDeleteRowSchema, null);
+
+        OutputFileFactory outputFileFactory =
+                OutputFileFactory.builderFor(table, 10000, 10086).format(FileFormat.PARQUET).build();
+
+        PartitionKey partitionKey = new PartitionKey(partitionSpec, schema);
+        partitionKey.set(0, "A");
+        EncryptedOutputFile outputFile = outputFileFactory.newOutputFile();
+        EqualityDeleteWriter<Record> equalityDeleteWriter =
+                genericAppenderFactory.newEqDeleteWriter(outputFile, FileFormat.PARQUET, partitionKey);
+
+        try (EqualityDeleteWriter<Record> closeableWriter = equalityDeleteWriter) {
+            GenericRecord record = GenericRecord.create(eqDeleteRowSchema);
+            for (int i = 4; i <= 6; ++i) {
+                closeableWriter.write(record.copy(ImmutableMap.of("contact_class", "A", "contact_id", i)));
+            }
+        }
+
+        DeleteFile deleteFile = equalityDeleteWriter.toDeleteFile();
+        LOG.info("Row delta - deleteByEquality2");
         table.newRowDelta().addDeletes(deleteFile).commit();
     }
 
